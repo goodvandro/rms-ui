85b3f8e4f1d782f935cc3526938670a0c806cf22
Evandro Monteiro <goodvandro@gmail.com>
2021-04-10 19:54:10 +0000

    set prodution settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://igf-api.herokuapp.com',
   basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
-
   allowedDomains: ['igf-api.herokuapp.com'],
   disallowedRoutes: ['https://igf-api.herokuapp.com/oauth/token'],
 };
