ff1091b530ecdbfdd264a15eea1fbdb1861ed84d
Evandro Monteiro <goodvandro@gmail.com>
2021-04-10 20:00:31 +0000

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.164:8081',
+  /*apiUrl: 'http://192.168.0.164:8081',
   allowedDomains: ['192.168.0.164:8081'],
   disallowedRoutes: ['http://192.168.0.164:8081/oauth/token'],
-  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA=='
+  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA=='*/
+
+  apiUrl: 'https://igf-api.herokuapp.com',
+  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
+  allowedDomains: ['igf-api.herokuapp.com'],
+  disallowedRoutes: ['https://igf-api.herokuapp.com/oauth/token'],
 };
