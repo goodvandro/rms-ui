db3b1ca0c2d051a1b56806537fcdf61a05fe1bc8
Evandro Monteiro <goodvandro@gmail.com>
2021-05-21 17:57:08 +0000

    index compliance

--- src/app/compliance/compliance.service.ts
+++ src/app/compliance/compliance.service.ts
@@ -1,9 +1,73 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { environment } from 'src/environments/environment';
+import { AppHttClient } from './../auth/app-http-client';
+import { Compliance } from './../models/compliance';
+
+export class ComplianceFilter {
+  page: number = 0;
+  rows: number = 10;
+
+  number: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComplianceService {
+  API_URL: string;
 
-  constructor() { }
+  constructor(private http: AppHttClient) {
+    this.API_URL = `${environment.apiUrl}/compliance`;
+  }
+
+  async create(compliance: Compliance): Promise<Compliance> {
+    return this.http.post<Compliance>(this.API_URL, compliance)
+      .toPromise();
+  }
+
+  async read(filter: ComplianceFilter): Promise<any> {
+    let params = new HttpParams();
+    params = params.append('page', filter.page.toString());
+    params = params.append('size', filter.rows.toString());
+
+    return this.http.get<any>(this.API_URL, { params })
+      .toPromise()
+      .then((result) => {
+        const compliances: Compliance[] = result.content;
+        this.convertField(compliances);
+        return {
+          content: compliances,
+          totalElements: result.totalElements
+        }
+      });
+  }
+
+  async update(id: number, compliance: Compliance): Promise<Compliance> {
+    return this.http.put<Compliance>(`${this.API_URL}`, compliance)
+      .toPromise()
+      .then((result => {
+        const compliance = result as Compliance;
+        this.convertField([compliance]);
+        return compliance;
+      }))
+  }
+
+  async getById(id: number): Promise<Compliance> {
+    return this.http.get<Compliance>(`${this.API_URL}/${id}`)
+      .toPromise()
+      .then((result => {
+        const compliance = result as Compliance;
+        this.convertField([compliance]);
+        return compliance;
+      }))
+  }
+
+  private convertField(compliances: Compliance[]) {
+    for (const compliance of compliances) {
+      compliance.createdAt = moment(compliance.createdAt, 'YYYY-MM-DD').toDate();
+      compliance.updatedAt = moment(compliance.updatedAt, 'YYYY-MM-DD').toDate();
+    }
+  }
 }
--- src/app/compliance/index/index.component.ts
+++ src/app/compliance/index/index.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api';
+import { ErrorService } from 'src/app/error/error.service';
+import { ComplianceFilter, ComplianceService } from '../compliance.service';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  loading: boolean;
+  filter = new ComplianceFilter();
+  totalRecords: number = 0;
+  compliances = [];
 
-  constructor() { }
+  constructor(
+    private complianceService: ComplianceService,
+    private errorService: ErrorService,
+  ) { }
 
   ngOnInit(): void {
+    this.loading = true;
   }
 
+  read(page = 0): void {
+    this.filter.page = page;
+
+    this.complianceService.read(this.filter)
+      .then((result) => {
+        this.compliances = result.content;
+        this.totalRecords = result.totalElements;
+      })
+      .catch((error) => this.errorService.handle(error))
+      .finally(() => this.loading = false);
+  }
+
+  lazyLoad(event: LazyLoadEvent) {
+    const page = event.first / event.rows;
+    this.filter.rows = event.rows;
+    this.read(page);
+  }
 }
