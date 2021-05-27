21e51945f7f2b9fc6ab217016c58596d690089cc
Evandro Monteiro <goodvandro@gmail.com>
2021-05-27 14:38:54 +0000

    design dashboard

--- src/app/start/index/index.component.ts
+++ src/app/start/index/index.component.ts
@@ -6,9 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  data: any;
+  basicData: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    };
+
+    this.basicData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Recomendações atribuídas',
+          backgroundColor: '#42A5F5',
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: 'Recomendações cumpridas',
+          backgroundColor: '#FFA726',
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
   }
 }
--- src/app/start/start.module.ts
+++ src/app/start/start.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StartRouter } from './start.router';
+import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
+import { IndexComponent } from './index/index.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StartRouter } from './start.router';
 
 @NgModule({
-  declarations: [PageNotFoundComponent],
+  declarations: [
+    PageNotFoundComponent,
+    IndexComponent,
+  ],
   imports: [
     CommonModule,
-    StartRouter
+    StartRouter,
+    CardModule,
+    ChartModule
   ]
 })
 export class StartModule { }
