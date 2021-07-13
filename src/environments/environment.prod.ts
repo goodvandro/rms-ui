44147a938f1f53ea48c2e0aa8d6e50dcc83edde8
Evandro Monteiro <goodvandro@gmail.com>
2021-07-13 15:52:48 +0000

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,8 @@
-export const app = {
-  developmentHost: '197.159.166.11:8081',
-  // host: '192.168.0.164:8081',
-  testingHost: 'igf-api.herokuapp.com',
-  stagingHost: 'webapp.b-software.st:8080'
-}
-
 export const environment = {
+
   production: true,
-
-  apiUrl: `http://${app.testingHost}`,
-  allowedDomains: [`${app.testingHost}`],
-  disallowedRoutes: [`http://${app.testingHost}/oauth/token`],
-  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA=='
-
-  // production: true,
-  // apiUrl: 'https://igf-api.herokuapp.com',
-  // basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
-  // allowedDomains: ['igf-api.herokuapp.com'],
-  // disallowedRoutes: ['https://igf-api.herokuapp.com/oauth/token'],
+  apiUrl: 'https://igf-api.herokuapp.com',
+  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
+  allowedDomains: ['igf-api.herokuapp.com'],
+  disallowedRoutes: ['https://igf-api.herokuapp.com/oauth/token'],
 };
