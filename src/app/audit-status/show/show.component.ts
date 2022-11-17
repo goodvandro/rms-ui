51216a1b56ad703c849e2525d95a4025b7538a6b
Evandro Monteiro Lima <evandro.lima@gat.digital>
2022-11-17 12:15:03 +0000

    Adjust audit status manager

--- src/app/audit-status/show/show.component.ts
+++ src/app/audit-status/show/show.component.ts
@@ -7,7 +7,7 @@ import { AuditStatusService } from './../audit-status.service';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.scss']
+  styleUrls: ['./show.component.scss'],
 })
 export class ShowComponent implements OnInit {
   @Input() auditStatus: AuditStatus;
@@ -19,22 +19,32 @@ export class ShowComponent implements OnInit {
     private auditStatusService: AuditStatusService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.auditStatus.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   update(): void {
-    this.auditStatusService.update(this.auditStatus.id, this.auditStatus)
+    this.auditStatusService
+      .update(this.auditStatus.id, this.auditStatus)
       .then(() => {
         this.display = false;
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
