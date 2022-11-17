51905c4c1620d91fdb6b8d0752b1a1877ee3edc2
Evandro Monteiro Lima <evandro.lima@gat.digital>
2022-11-17 12:34:04 +0000

    Adjust entity level manager

--- src/app/entity-level/new/new.component.ts
+++ src/app/entity-level/new/new.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.scss']
+  styleUrls: ['./new.component.scss'],
 })
 export class NewComponent implements OnInit {
   @Input() entityLevels = [];
@@ -21,17 +21,27 @@ export class NewComponent implements OnInit {
     private entityLevelService: EntityLevelService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.entityLevel.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   create(form: NgForm): void {
-    this.entityLevelService.create(this.entityLevel)
+    this.entityLevelService
+      .create(this.entityLevel)
       .then((result) => {
         this.entityLevels.push(result);
         this.display = false;
@@ -39,6 +49,6 @@ export class NewComponent implements OnInit {
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
--- src/app/entity-level/show/show.component.ts
+++ src/app/entity-level/show/show.component.ts
@@ -7,7 +7,7 @@ import { EntityLevel } from './../../models/entity-level';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.scss']
+  styleUrls: ['./show.component.scss'],
 })
 export class ShowComponent implements OnInit {
   @Input() entityLevel: EntityLevel;
@@ -19,22 +19,32 @@ export class ShowComponent implements OnInit {
     private entityLevelService: EntityLevelService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.entityLevel.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   update(): void {
-    this.entityLevelService.update(this.entityLevel.id, this.entityLevel)
+    this.entityLevelService
+      .update(this.entityLevel.id, this.entityLevel)
       .then(() => {
         this.display = false;
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
