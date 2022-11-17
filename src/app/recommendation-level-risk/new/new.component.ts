60b9cd347a72447eb0be3632493dca0e803fcbb7
Evandro Monteiro Lima <evandro.lima@gat.digital>
2022-11-17 08:56:32 +0000

    Adjust recommendation level risk manager

--- src/app/recommendation-level-risk/new/new.component.ts
+++ src/app/recommendation-level-risk/new/new.component.ts
@@ -8,7 +8,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.scss']
+  styleUrls: ['./new.component.scss'],
 })
 export class NewComponent implements OnInit {
   @Input() levelsRisk = [];
@@ -21,17 +21,27 @@ export class NewComponent implements OnInit {
     private recommendationLevelRiskService: RecommendationLevelRiskService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.levelRisk.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   create(form: NgForm): void {
-    this.recommendationLevelRiskService.create(this.levelRisk)
+    this.recommendationLevelRiskService
+      .create(this.levelRisk)
       .then((result) => {
         this.levelsRisk.push(result);
         this.display = false;
@@ -39,6 +49,6 @@ export class NewComponent implements OnInit {
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
--- src/app/recommendation-level-risk/show/show.component.ts
+++ src/app/recommendation-level-risk/show/show.component.ts
@@ -7,7 +7,7 @@ import { RecommendationLevelRisk } from 'src/app/models/recommendation-level-ris
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.scss']
+  styleUrls: ['./show.component.scss'],
 })
 export class ShowComponent implements OnInit {
   @Input() levelRisk: RecommendationLevelRisk;
@@ -19,22 +19,32 @@ export class ShowComponent implements OnInit {
     private recommendationLevelRiskService: RecommendationLevelRiskService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.levelRisk.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   update(): void {
-    this.recommendationLevelRiskService.update(this.levelRisk.id, this.levelRisk)
+    this.recommendationLevelRiskService
+      .update(this.levelRisk.id, this.levelRisk)
       .then(() => {
         this.display = false;
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
