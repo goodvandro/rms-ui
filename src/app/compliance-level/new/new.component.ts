48f1d1fc814399528077cfbf937197b99694e817
Evandro Monteiro Lima <evandro.lima@gat.digital>
2022-11-17 12:30:07 +0000

    Adjust manager compliance level

--- src/app/compliance-level/new/new.component.ts
+++ src/app/compliance-level/new/new.component.ts
@@ -8,7 +8,7 @@ import { ComplianceLevelService } from './../compliance-level.service';
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
-  styleUrls: ['./new.component.scss']
+  styleUrls: ['./new.component.scss'],
 })
 export class NewComponent implements OnInit {
   @Input() complianceLevels = [];
@@ -21,17 +21,27 @@ export class NewComponent implements OnInit {
     private complianceLevelService: ComplianceLevelService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.complianceLevel.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   create(form: NgForm): void {
-    this.complianceLevelService.create(this.complianceLevel)
+    this.complianceLevelService
+      .create(this.complianceLevel)
       .then((result) => {
         this.complianceLevels.push(result);
         this.display = false;
@@ -39,6 +49,6 @@ export class NewComponent implements OnInit {
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
--- src/app/compliance-level/show/show.component.ts
+++ src/app/compliance-level/show/show.component.ts
@@ -7,7 +7,7 @@ import { ComplianceLevelService } from './../compliance-level.service';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.scss']
+  styleUrls: ['./show.component.scss'],
 })
 export class ShowComponent implements OnInit {
   @Input() complianceLevel: ComplianceLevel;
@@ -19,22 +19,32 @@ export class ShowComponent implements OnInit {
     private complianceLevelService: ComplianceLevelService,
     private errorService: ErrorService,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   showDialog() {
     this.display = true;
   }
 
+  generateSlug(event: any) {
+    const name: string = event.target.value;
+
+    this.complianceLevel.slug = name
+      .replace(/\s/g, '_')
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
   update(): void {
-    this.complianceLevelService.update(this.complianceLevel.id, this.complianceLevel)
+    this.complianceLevelService
+      .update(this.complianceLevel.id, this.complianceLevel)
       .then(() => {
         this.display = false;
         this.toastr.success('Informações salvas!');
       })
       .catch((error) => this.errorService.handle(error))
-      .finally(() => this.loading = false)
+      .finally(() => (this.loading = false));
   }
 }
