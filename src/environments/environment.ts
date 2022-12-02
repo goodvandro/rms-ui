b3b4e194f12e52c0f275be6fbbeaeb040c7cb284
Evandro Monteiro Lima <evandro.lima@financas.st>
2022-12-02 14:58:59 +0000

    Setup enviroment to staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,20 @@
 export const app = {
   // developmentHost: '197.159.166.11:8081',
-  developmentHost: '192.168.0.136:8081',
+  developmentHost: '192.168.0.164:8081',
   testingHost: 'igf-api.herokuapp.com',
-  stagingHost: 'webapp.b-software.st:8080',
+  stagingHost: '52.204.190.168:8083',
 };
 
 export const environment = {
-  production: false,
-  apiUrl: `http://${app.developmentHost}`,
-  allowedDomains: [`${app.developmentHost}`],
-  disallowedRoutes: [`http://${app.developmentHost}/oauth/token`],
-  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
-
+  // production: true,
   // apiUrl: 'https://igf-api.herokuapp.com',
   // basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
   // allowedDomains: ['igf-api.herokuapp.com'],
   // disallowedRoutes: ['https://igf-api.herokuapp.com/oauth/token'],
+
+  production: true,
+  apiUrl: `http://${app.testingHost}`,
+  allowedDomains: [`${app.testingHost}`],
+  disallowedRoutes: [`http://${app.testingHost}/oauth/token`],
+  basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
 };
