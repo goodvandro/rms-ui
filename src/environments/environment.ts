0abb500b7531eeed9d7879d8a8c1b8c29c038df9
Evandro Monteiro <goodvandro@gmail.com>
2022-12-07 15:13:35 +0000

    Set render api enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,12 @@
 export const app = {
-  // developmentHost: '197.159.166.11:8081',
   developmentHost: '192.168.0.164:8081',
-  testingHost: 'igf-api.herokuapp.com',
-  stagingHost: '52.204.190.168:8083',
+  stagingHost: 'igf-api.onrender.com',
 };
 
 export const environment = {
-  // production: true,
-  // apiUrl: 'https://igf-api.herokuapp.com',
-  // basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
-  // allowedDomains: ['igf-api.herokuapp.com'],
-  // disallowedRoutes: ['https://igf-api.herokuapp.com/oauth/token'],
-
   production: true,
-  apiUrl: `http://${app.stagingHost}`,
+  apiUrl: `https://${app.stagingHost}`,
   allowedDomains: [`${app.stagingHost}`],
-  disallowedRoutes: [`http://${app.stagingHost}/oauth/token`],
+  disallowedRoutes: [`https://${app.stagingHost}/oauth/token`],
   basicAuthorization: 'Basic YW5ndWxhcjpAbmd1bEByMA==',
 };
